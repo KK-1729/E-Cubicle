f2ea9ca951332e3f66370bd5e9b1626a93d1affb
BhargavaramMuppuru <56501891+BhargavaramMuppuru@users.noreply.github.com>
2021-05-15 12:43:39 +0530

    Features

---
+++ script.js
@@ -0,0 +1,108 @@
+const classifier = knnClassifier.create();
+const webcamElement = document.getElementById('webcam');
+let net;
+var audio = new Audio('audio_file.mp3');
+
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  await setupWebcam();
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(webcamElement, 'conv_preds');
+      // Get the most likely class and confidences from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['A', 'B'];
+      if(classes[result.classIndex]=="B"){
+        await audio.play();
+        document.body.style.backgroundColor = "rgb(168, 63, 63)";
+      }
+      else{
+        document.body.style.backgroundColor = "rgb(80, 168, 80)";
+      }
+    }
+
+    await tf.nextFrame();
+  }
+}
+
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
+app();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var modeSwitch = document.querySelector('.mode-switch');
+
+  modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+    modeSwitch.classList.toggle('active');
+  });
+  
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+  
+  listView.addEventListener('click', function () {
+    gridView.classList.remove('active');
+    listView.classList.add('active');
+    projectsList.classList.remove('jsGridView');
+    projectsList.classList.add('jsListView');
+  });
+  
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+  
+  document.querySelector('.messages-btn').addEventListener('click', function () {
+    document.querySelector('.messages-section').classList.add('show');
+  });
+  
+  document.querySelector('.messages-close').addEventListener('click', function() {
+    document.querySelector('.messages-section').classList.remove('show');
+  });
+});
+
